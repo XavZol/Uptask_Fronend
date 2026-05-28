0d0fff0a3d3ed0414f093ca8ac8ef11a694fd780
XavZol <alansolis707@gmail.com>
2026-05-28 07:07:26 -0600

    saliendo

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import './index.css'
 import Router from './router'
-import process from 'dotenv';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // Data is considered fresh for 5 minutes
-      cacheTime: 1000 * 60 * 60 * 24, // Data stays in cache for 24 hours
       refetchOnWindowFocus: false, // Do not refetch on window focus by default
     },
   },
