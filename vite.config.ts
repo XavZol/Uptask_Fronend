f05f021297e86f79886d74d85444cd8cf4e1033d
XavZol <alansolis707@gmail.com>
2026-06-12 11:41:34 -0600

    Actualizar vite.config.ts
    chunk

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000 // Increases limit to 1000 kB
     rollupOptions: {
       output: {
         manualChunks(id) {
